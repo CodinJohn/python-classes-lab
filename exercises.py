142d1cbd369ab6a86fd74641de9df0cafb91170f
CodinJohn <johnathon.collado@gmail.com>
2024-11-25 16:58:44 -0500

    Lab Completed

--- exercises.py
+++ exercises.py
@@ -14,3 +14,91 @@ class Game():
             'b3': None, 
             'c3': None,
         }
+
+    def play_game(self):
+        print('Welcome gamers!')
+        print()
+        while not self.winner and not self.tie:
+            self.render()
+            self.get_move()
+            self.check_for_winner()            
+            self.check_for_tie()            
+            self.switch_turn()
+        self.render()
+        if self.winner:
+            print(f'Congrats! {self.winner} wins!')
+        elif self.tie:
+            print('You tied!')
+
+    def print_board(self):
+        b = self.board
+        print(f"""
+              A   B   C
+          1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+              ----------
+          2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+              ----------
+          3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    def print_message(self):
+        if (self.tie):
+            print('You tied!')
+        elif (self.winner):
+            print(f'{self.winner} wins!')
+        else:
+            print(f'Player {self.turn}\'s turn')    
+    
+    def render(self):
+        self.print_board()
+        self.print_message()
+    
+    def get_move(self):
+        if (not self.tie) or (self.winner == None):  
+            move = input(f"Enter a valid move (example: A1): ").lower()
+        if move in self.board and not self.board[move]:
+            self.board[move] = self.turn
+            print(f'You picked {move}')
+        else:
+            print(f"{move} is not a valid!")
+            
+
+    def check_for_winner(self):
+        if self.board['a1'] and (self.board['a1'] == 
+           self.board['b1'] == self.board['c1']):
+           self.winner = self.turn
+        elif self.board['a2'] and (self.board['a2'] == 
+             self.board['b2'] == self.board['c2']):
+             self.winner = self.turn        
+        elif self.board['a3'] and (self.board['a3'] == 
+             self.board['b3'] == self.board['c3']):
+             self.winner = self.turn 
+        elif self.board['a1'] and (self.board['a1'] == 
+             self.board['a2'] == self.board['a3']):
+             self.winner = self.turn 
+        elif self.board['b1'] and (self.board['b1'] == 
+             self.board['b2'] == self.board['b3']):
+             self.winner = self.turn 
+        elif self.board['c1'] and (self.board['c1'] == 
+             self.board['c2'] == self.board['c3']):
+             self.winner = self.turn 
+
+    def check_for_tie(self):
+        spaces_filled = 0        
+        for space in self.board:
+            if self.board[space] == None and not self.winner:
+                pass
+            else:
+                spaces_filled += 1       
+
+        if spaces_filled == 9 and not self.winner:
+            self.tie = True
+                
+    def switch_turn(self):
+        if self.turn == 'X':
+           self.turn = 'O'
+        elif self.turn == 'O':
+             self.turn = 'X'
+
+game_instance = Game()
+game_instance.play_game()
